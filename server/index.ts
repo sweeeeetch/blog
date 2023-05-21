4a0f2a5dd7cacaf9e5572b0283ca905c436a2f69
Ivan <76837657+sweeeeetch@users.noreply.github.com>
2023-05-21 21:48:01 +1000

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ export const prisma = new PrismaClient();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.CLIENT_URL as string, "http://localhost:5173"], // process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL, 
   })
 );
 app.use(express.json());
