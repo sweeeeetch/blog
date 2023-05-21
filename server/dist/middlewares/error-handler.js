d5674f6c321a91c6ac2dd8b222c26b68f5ac8f06
Ivan Sinyakov <bglaze@mail.ru>
2023-05-22 03:57:19 +1000

    minor changes

--- server/dist/middlewares/error-handler.js
+++
@@ -1,8 +0,0 @@
-import ApiError from "../exceptions/api-error.js";
-export default function (err, req, res, next) {
-    console.log(err);
-    if (err instanceof ApiError) {
-        return res.status(err.status).json({ message: err.message, errors: err.errors });
-    }
-    return res.sendStatus(500);
-}
