7935914014248d27cbbcfa5d8bdcecbec62e67bc
Ivan Sinyakov <bglaze@mail.ru>
2023-05-22 00:10:01 +1000

    minor fixes

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -8,7 +8,15 @@ const $api = axios.create({
 
 $api.interceptors.request.use(config => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
-  config.headers["Access-Control-Allow-Headers"] = "*";
+  config.headers["Access-Control-Allow-Headers"] = [
+    "Authorization",
+    "Content-Type",
+    "Set-Cookie",
+    "Accept",
+    "X-Requested-With",
+    "X-CSRF-Token",
+    "Cache-Control",
+  ];
   config.withCredentials = true;
   return config;
 });
