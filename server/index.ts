34784008b670925a3cb1602bfebc68df3fcf4d0d
Ivan Sinyakov <bglaze@mail.ru>
2023-05-22 16:57:14 +1000

    minor changes

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__filename);
 
 dotenv.config();
 const app = express();
