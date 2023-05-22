165d637404311c722daa6d52d979b981833bc89a
Ivan Sinyakov <bglaze@mail.ru>
2023-05-22 22:05:17 +1000

    minor fixes

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,12 @@ import fileUpload from "express-fileupload";
 import router from "./router/index.js";
 import { PrismaClient } from "@prisma/client";
 import errorHandler from "./middlewares/error-handler.js";
-import staticMiddleware from "./middlewares/staticMiddleware.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename);
 
 dotenv.config();
 const app = express();
@@ -20,14 +25,19 @@ app.use(
     exposedHeaders: ["set-cookie"],
   })
 );
-app.use("/static", staticMiddleware);
 app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
-
 app.get("/healthz", (req, res) => {
   return res.sendStatus(200);
+});
+
+app.get("/static/:file", async (req, res) => {
+  // i had some troubles with express.static. it didnt sent images at all. i didnt find out what was the problem, so i made my own static serve
+  const { file } = req.params;
+  const imagePath = path.join(__dirname, "static", file);
+  return res.sendFile(imagePath);
 });
 
 app.use("/api", router);
